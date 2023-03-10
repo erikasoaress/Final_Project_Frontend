6705429d56a58e035de052fe506683e321a6b876
unknown <contacto@erikazdesign.com>
2023-03-10 19:09:36 +0100

    explorepage

--- pages/Explore.jsx
+++ pages/Explore.jsx
@@ -1,29 +1,26 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Navbar from '../src/components/Navbar';
+
 import { Link } from 'react-router-dom';
-import RadiosCountry from './RadiosCountry';
-import RadiosGenre from './RadiosGenre';
-import RadiosRanked from './RadiosRanked';
-
- const [radios, setRadios] = useState([]);
-
- const getRadios = async () => {
-   try {
-     const response = await radioService.GetRadios();
-     console.log(response.data);
-     setRadios(response.data);
-   } catch (error) {
-     console.log(error);
-   }
-
-   useEffect(() => {
-     getRadios;
-   }, []);
- };
 
 
 function Explore() {
+
+  const [radios, setRadios] = useState([]);
+
+  const getRadios = async () => {
+    try {
+      const response = await radioService.GetRadios();
+      console.log(response.data);
+      setRadios(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+ 
+    useEffect(() => {
+      getRadios;
+    }, []);
+  };
   return (
     <>
       <Link to={`/explore/${radios.country}`}> Search the radio by country</Link>
