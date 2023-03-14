b5144f61805ebff7cc73299b02ffddfe9a6899dd
Assebe <Lambadina1>
2023-03-14 15:27:53 +0000

    edited routes

--- pages/RadiosCountry.jsx
+++ pages/RadiosCountry.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
 import axios from "axios";
+import { useState, useEffect } from "react";
+
+function RadiosByCountry() {
+  const [radiosByCountry, setRadiosByCountry] = useState([]);
+    const { countryId } = useParams();
 
 
-function RadiosCountry() {
+  const getRadiosByCountry = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/api/radios/country/${countryId}`
+      );
+      setRadiosByCountry(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getRadiosByCountry();
+    console.log("a");
+  }, []);
+
+  useEffect(() => {
+    console.log(radiosByCountry);
+  }, [radiosByCountry]);
+
   return (
-    <div>RadiosCountry</div>
-  )
+    <div>
+      {radiosByCountry.map((radio) => {
+        return (
+          <div className="radio-wrapper">
+            <h3>{radio.country}</h3>
+            <h1>{radio.name}</h1>
+            <img
+              src={`${radio.favicon}`}
+              alt="radio symbol"
+              style={{ width: "250px" }}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default RadiosCountry
+export default RadiosByCountry;
--- pages/RadiosGenre.jsx
+++ pages/RadiosGenre.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
 import axios from "axios";
+import { useState, useEffect } from "react";
+
+function RadiosByGenre() {
+  const [radiosByGenre, setRadiosByGenre] = useState([]);
+  const { genreId } = useParams();
 
 
-function RadiosGenre() {
+  const getRadiosByGenre = async () => {
+
+    try {
+        const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/api/radios/genre/${genreId}`
+      );
+      setRadiosByGenre(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getRadiosByGenre();
+    console.log("a");
+  }, []);
+
+  useEffect(() => {
+    console.log(radiosByGenre);
+  }, [radiosByGenre]);
+
   return (
-    <div>RadiosGenre</div>
-  )
+    <div>
+      {radiosByGenre.map((radio) => {
+        return (
+          <div className="radio-wrapper">
+            <h3>{radio.country}</h3>
+            <h1>{radio.name}</h1>
+            <img
+              src={`${radio.favicon}`}
+              alt="radio symbol"
+              style={{ width: "250px" }}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default RadiosGenre
+export default RadiosByGenre;
