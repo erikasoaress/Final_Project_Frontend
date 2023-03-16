b9ae6c424900215956739a97e65805b77f3819d0
Assebe <Lambadina1>
2023-03-16 11:18:58 +0000

    changes

--- src/components/searchbar/SearchbarHeader.jsx
+++ src/components/searchbar/SearchbarHeader.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import RadioCards from "../radio-cards/RadioCards";
-import img from './magnifyingglass.png'
+
 
 function SearchbarHeader() {
   const [search, setSearch] = useState("");
