b8b35e3cf6198449d82a63f9d33f088243e4cc0c
Assebe <Lambadina1>
2023-03-16 17:46:13 +0000

    final

--- pages/Profile.jsx
+++ pages/Profile.jsx
@@ -3,6 +3,7 @@ import { AuthContext } from "../src/context/auth.context";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./profile.css";
+import { Link } from "react-router-dom";
 import FavoriteRadios from "../src/components/favorites/FavoriteRadios";
 
 function Profile() {
